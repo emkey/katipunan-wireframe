0e22a490492052b3321427d0d0b73a6aa41b5007
emkey <mllacsamana@gmail.com>
2015-09-24 15:34:08 +0800

    added top bar navigation with search bar and cleaned up ad details

---
+++ topbar.php
@@ -0,0 +1,43 @@
+<!-- topbar -->
+<div class="fixed">
+	<nav class="top-bar" data-topbar role="navigation">
+	  <ul class="title-area">
+	    <li class="name">
+	      <h1><a href="#">My Site</a></h1>
+	    </li>
+	    <li class="has-form">
+	      <div class="row collapse">
+	        <div class="large-8 small-9 columns">
+	          <input type="text" placeholder="Find Stuff">
+	        </div>
+	        <div class="large-4 small-3 columns">
+	          <a href="#" class="alert button expand">Search</a>
+	        </div>
+	      </div>
+	    </li>
+	     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+		 <!--<li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li> -->
+	  </ul>
+	
+	  <section class="top-bar-section">
+	    <!-- Right Nav Section -->
+	    <!-- 
+	    <ul class="right">
+	      <li><a href="#">Button</a></li>
+	      <li class="has-dropdown">
+	        <a href="#">Dropdown</a>
+	        <ul class="dropdown">
+	          <li><a href="#">link</a></li>
+	          <li><a href="#">link</a></li>
+	        </ul>
+	      </li>
+	    </ul>
+		-->
+	    <!-- Left Nav Section -->
+	    <ul class="left">
+	      
+	    </ul>
+	  </section>
+	</nav>
+</div>
+<!-- end topbar -->
