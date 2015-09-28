fe02c24b7976ef1f035fa6fd166b4db77ff7a17c
emkey <mllacsamana@gmail.com>
2015-09-28 12:53:56 +0800

    created medium and large ad listings view

--- ad-listings.php
+++ ad-listings.php
@@ -9,8 +9,11 @@
 	</head>
 	<body>
 <?php include('topbar.php'); ?>
+	<div id="searchFilter" class="fixedButton hide-for-large-up">
+		<a href="#" class="button expand">Filter</a>
+	</div>
 <!-- Ad Content -->
-	<div class="row small-collapse">
+	<div class="row small-collapse  medium-uncollapse large-uncollapse">
 		<div class="small-12 columns">
 			<ul class="breadcrumbs" style="margin-bottom: 0px;">
 			<li><a href="#">Home</a></li>
@@ -18,28 +21,31 @@
 			</ul>
 		</div>
 	</div>
-	<div class="row">
+	<div class="row adListings">
 		<div class="small-12 columns">
+			<div class="row  small-collapse medium-uncollapse large-uncollapse">
 <?php
-for ($i = 0; $i < 10; $i++) {
-?>
-			<div class="row adListing">
-				<div class="small-4 columns ad-thumbnail">
-					
-				</div>
-				<div class="small-8 columns">
-					<div class="adTitle"><a href="">Wow such ad title much sell very yaman</a></div>
-					<div class="adPrice">P9,990</div>
-					<div class="adMeta">
-						<small>Manila</small><br />
-						<small>Today 08:10</small>
+for ($i = 0; $i < 12; $i++) {
+?>			
+			<div class="small-12 medium-6 large-4 columns">
+				<div class="row adListing">
+					<div class="small-4 large-12 columns ad-thumbnail">
+						
+					</div>
+					<div class="small-8 large-12 columns">
+						<div class="adTitle"><a href="">Wow such ad title much sell very yaman</a></div>
+						<div class="adPrice">P9,990</div>
+						<div class="adMeta">
+							<small>Manila</small><br />
+							<small>Today 08:10</small>
+						</div>
 					</div>
 				</div>
 			</div>
 <?php
 }
 ?>
-			
+			</div>
 		</div>
 	</div>
 <!-- end of ad content -->
