4b17b2d3dbe024108617c7c8a4cf953efb1f0808
emkey <mllacsamana@gmail.com>
2015-10-02 19:11:51 +0800

    git started on production level studies and base atoms

--- globals.php
+++ globals.php
@@ -9,9 +9,34 @@
 	</head>
 	<body>
 	<?php include('topbar.php'); ?>
+		<div class="row">
+			<div class="column small-12">
+				<p>
+					Ayon, ang kanyang pinaka-pagoda cold wave lotion mong mga vaklush ng hangin ay nagpa-feel ng mga chuchubels ng bato! O ano, ang aking napaka-ganders na mga hitad ng mother ay umapear ng mga beki ng anik-anik! At in fairview, ang ating plastikada mong mga kapatid ng dilim ay nag-jembot-jembot ng mga tagumpay ng chenelyn!
+				</p>
+				<p>
+		Hay nako, ang iyong ganders mong mga lokarets ng kaibigan ay tinangkilik ng mga eklavoo ng itech. At eto pa, ang ating kyoho mong mga keri ng pangalan ay na-Julie Yap Daza ng mga reynabels ng tagumpay! Hay nako, ang kanyang napaka-plastikada na mga lokarets ng chuchu ay lumafang ng mga itech ng reynabels.
+				</p>
+				<p>
+					<strong>Ayon, ang kanyang pinaka-pagoda cold wave lotion mong mga vaklush ng hangin ay nagpa-feel ng mga chuchubels ng bato!</strong>
+				</p>
+				<h1>Header 1</h1>
+				<h2>Header 2</h2>
+				<h3>Header 3</h3>
+				<h4>Header 4</h4>
+				<h5>Header 5</h5>
+				<h6>Header 6</h6>
+				<h1><strong>Header 1</strong></h1>
+				<h2><strong>Header 2</strong></h2>
+				<h3><strong>Header 3</strong></h3>
+				<h4><strong>Header 4</strong></h4>
+				<h5><strong>Header 5</strong></h5>
+				<h6><strong>Header 6</strong></h6>
+				<p><a href="#">This is a Link</a></p>
+			</div>
+		</div>
+	<?php include ('footer.php'); ?>
+	<?php include('scripts.php'); ?>
 
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/foundation/js/foundation.min.js"></script>
-    <script src="js/app.js"></script>
   </body>
 </html>
