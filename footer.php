f5faa38dd0884646a805ff369ab8e0cc214a5228
emkey <mllacsamana@gmail.com>
2015-10-05 16:17:16 +0800

    added footer

--- footer.php
+++ footer.php
@@ -1,3 +1,26 @@
-<footer>
-	The footer goes here
+<footer id="global-footer">
+	<div class="row">
+		<div class="small-12 medium-4 columns">
+			<h2>OLX</h2>
+			<ul>
+				<li><a href="http://yaman.olx.ph">Blog</a></li>
+				<li><a href="#">Help</a></li>
+				<li><a href="">Privacy Policy</a></li>
+			</ul>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<h2>Partners</h2>
+			<ul>
+				<li><a href="">OLX Indonesia</a></li>
+				<li><a href="">Property 24</a></li>
+			</ul>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<a href=""><img src="images/androidstore-badge.png" alt="androidstore-badge" width="129" height="45" /></a>
+			<a href=""><img src="images/applestore-badge.png" alt="applestore-badge" width="135" height="40" /></a>
+		</div>
+		<div class="small-12 columns copyright">
+			Copyright &copy; 2006-2015 <a href="">OLX Philippines</a>
+		</div>
+	</div>
 </footer>
