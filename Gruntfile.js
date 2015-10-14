277b64fef16f237a3ba1aaaab69ce67ae77d58b6
emkey <mllacsamana@gmail.com>
2015-10-14 17:24:10 +0800

    changed libsass compilation from compressed to expanded

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       },
       dist: {
         options: {
-          outputStyle: 'compressed',
+          outputStyle: 'expanded',
           sourceMap: true,
         },
         files: {
