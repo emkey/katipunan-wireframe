5e9323bd5ca6be2e6f23353ea65bc3c79dc64e59
emkey <mllacsamana@gmail.com>
2015-10-16 11:38:05 +0800

    Fixed duplication of code load using offsets

--- ad-details.php
+++ ad-details.php
@@ -13,12 +13,25 @@
 	
 	<div class="row">
 		<header class="small-12 column"><h1 class="ad-title">Lorem Ipsum Dolor Sit Amet</h1></header>
-		<div class="gallery small-12 large-8 column">
-			<img src="http://placehold.it/640x360">
+		<div class="gallery small-12 medium-12 large-8 large-reset-order column">
+			<div class="orbit-container">
+				<ul class="example-orbit-content" data-orbit>
+					<li data-orbit-slide="headline-1">
+						<img src="http://placehold.it/640x360">
+					</li>
+					<li data-orbit-slide="headline-2">
+						<img src="http://placehold.it/640x360">
+					</li>
+					<li data-orbit-slide="headline-3">
+						<img src="http://placehold.it/640x360">
+					</li>
+				</ul>
+			</div>
 		</div>
-		
-		<div class="small-12 medium-5 large-4 column medium-push-7 large-reset-order">
-			<div class="card">
+
+
+		<div class="small-12 medium-5 medium-push-7 large-4 large-reset-order  column">
+			<div class="card" id="ad-data">
 				<header class="card-header">
 					<small>22 Views</small>
 				</header>
@@ -36,7 +49,7 @@
 						
 						<div class="data-set">
 							<div class="data-label">
-								<div class="label">Contact Number</div>
+								<div class="label">Contact</div>
 							</div>
 							<div class="data-content">
 								<a href="" class="button success">0917-6315010</a>
@@ -48,7 +61,7 @@
 								Date Posted
 							</div>
 							<div class="data-content">
-								Oct 12, 2015
+								<small>Oct 12, 2015</small>
 							</div>
 						</div>
 						
@@ -57,7 +70,7 @@
 								Condition
 							</div>
 							<div class="data-content">
-								2nd Hand (used)
+								<small>2nd Hand (used)</small>
 							</div>
 						</div>
 						
@@ -66,90 +79,100 @@
 								Location
 							</div>
 							<div class="data-content">
-								<a href="">Taguig City, Metro Manila</a>
+								<small><a href="">Taguig City, Metro Manila</a></small>
 							</div>
 						</div>
 	
 						<div class="data-set">
 							<div class="data-label">
-									Ad ID
-								</div>
+								Ad ID
+							</div>
 							<div class="data-content">
-									123456
-								</div>
+								<small>123456</small>
+							</div>
 						</div>
 						
 						<a href="#" data-reveal-id="sendPM" class="button hollow display-block text-center">Send a Private Message</a>
-						<div id="sendPM" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
-							 <a class="close-reveal-modal" aria-label="Close">&#215;</a>
-							<h2>Contact</h2>
-							<form>
-								<textarea>
-									Enter your message
-								</textarea>
-								<input type="submit" class="button display-block success" value="Send Message" />
-							</form>
-						</div>
+						
+							<div id="sendPM" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+								 <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+								<h2>Contact</h2>
+								<form>
+									<textarea>
+										Enter your message
+									</textarea>
+									<input type="submit" class="button display-block success" value="Send Message" />
+								</form>
+							</div>
 					</div>
 				</div>
-			</div>
-			<div class="card">
-				<header class="card-header">
-				Seller Info
-			</header>
-				<div class="card-body">
-					<div class="data-set">
-						<div class="data-label">
-							<div class="label">Username</div>
-						</div>
-						<div class="small-7 column data-content">
-							<a href="#"><strong>mllacsamana</strong></a>
-						</div>
-						<div class="data-label">
-							<div class="label">User Address</div>
-						</div>
-						<div class="small-7 column data-content">
-							<small>Manila, Metro Manila</small>
-						</div>
-						<div class="data-label">
-							<div class="label">Last Accessed</div>
-						</div>
-						<div class="small-7 column data-content">
-							<small>Philippines</small>
-						</div>
-						
-						<div class="data-label">
-							<div class="label">Registered</div>
-						</div>
-						<div class="small-7 column data-content">
-							<small>14 October 2003</small>
-						</div>
-						
-						<div class="data-label">
-							<div class="label">Last Login</div>
-						</div>
-						<div class="small-7 column data-content">
-							<small>6 days ago</small>
-						</div>
-						
-					</div>
-				</div>
-			</div>
-		</div>	
-		
-		
-		
-		<div class="card small-12 medium-7 large-8 column end medium-pull-5 large-reset-order">
-			<header class="card-header">
-				<small>Description</small>
-			</header>
-			<div class="card-body">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<a class="button hollow display-block text-center">
-					Read More
-				</a>
 			</div>
 		</div>
+	
+		
+
+		<div class="small-12 medium-7 medium-pull-5 large-8 large-reset-order  column">
+			<div class="card description">
+				<header class="card-header">
+					<small>Description</small>
+				</header>
+				<div class="card-body">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<a class="button hollow display-block text-center">
+						Read More
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="card small-12 medium-5 large-4 medium-offset-7 large-offset-0 large-reset-order column" id="seller-data">
+			<header class="card-header">
+					Seller Info
+			</header>
+			<div class="card-body">
+				<div class="data-set">
+					
+					<div class="data-label">
+						<div class="label">Username</div>
+					</div>
+					<div class="small-7 column data-content">
+						<a href="#"><strong>mllacsamana</strong></a>
+					</div>
+					
+					<div class="data-label">
+						<div class="label">User Address</div>
+					</div>
+					<div class="small-7 column data-content">
+						<small>Manila, Metro Manila</small>
+					</div>
+					
+					<div class="data-label">
+						<div class="label">Last Accessed</div>
+					</div>
+					<div class="small-7 column data-content">
+						<small>Philippines</small>
+					</div>
+					
+					<div class="data-label">
+						<div class="label">Registered</div>
+					</div>
+					<div class="small-7 column data-content">
+						<small>14 October 2003</small>
+					</div>
+					
+					<div class="data-label">
+						<div class="label">Last Login</div>
+					</div>
+					<div class="small-7 column data-content">
+						<small>6 days ago</small>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	
+
+
 	</div>
 	
 	<div class="contact-bar hide-for-large-up">
