6731e4728ea534c70f9d6b984d789a364088f2cb
emkey <mllacsamana@gmail.com>
2015-10-27 11:35:07 +0800

    added modifications modals on ad listings

--- ad-listings.php
+++ ad-listings.php
@@ -16,10 +16,10 @@
         <!-- whatever you want goes here -->
 		<ul class="off-canvas-list">
 			<li><label>Filters</label></li>
-			<li><a href="#" data-reveal-id="filterModal">Change Categories</a></li>
-			<li><a href="#">Locations</a></li>
-			<li><a href="#">Price Range</a></li>
-			<li><a href="#">Date Posted</a></li>
+			<li><a href="#" data-reveal-id="filterModal-sort">Sort by: Cheapest First</a></li>
+			<li><a href="#" data-reveal-id="filterModal-category">Change Category</a></li>
+			<li><a href="#" data-reveal-id="filterModal-location">Change Location</a></li>
+			<li><a href="#" data-reveal-id="filterModal-appPush">Advanced Filter</a></li>
 		</ul>
     </aside>
 		<div class="filter-fixed-bar hide-for-large-up">
@@ -27,11 +27,36 @@
 				<a href="#" class="left-off-canvas-toggle filter-button button display-block text-center">Filter</a>
 			</div>
 		</div>
-		<div id="filterModal" class="reveal-modal" data-reveal aria-labelledby="filterListings" aria-hidden="true" role="dialog">
-			<h2 id="modalTitle">Filters</h2>
+		<!-- start of category modals -->
+		<!-- this shows the first level categories -->
+		<div id="filterModal-category" class="reveal-modal" data-reveal aria-labelledby="filterCategories" aria-hidden="true" role="dialog">
+			<h2 id="filterCategories">Categories</h2>
+			<div class="list-category">
+				<div class="card-category">
+					<a href="#">Category A</a>
+				</div>
+				<div class="card-category">
+					<a href="#">Category B</a>
+				</div>
+				<div class="card-category">
+					<a href="#">Category C</a>
+				</div>
+			</div>
 			<a class="close-reveal-modal" aria-label="Close">&#215;</a>
-			
 		</div>
+		
+		
+		<!-- Sort by Modal -->
+		<div id="filterModal-sort" class="reveal-modal tiny" data-reveal aria-labelledby="filterModal-sortBy" aria-hidden="true" role="dialog">
+			<h2 id="filterModal-sortBy">Sort By</h2>
+			<a class="close-reveal-modal" aria-label="Close">&#215;</a>
+			<a href="" class="button display-block text-center hollow">Cheapest First</a>
+			<a href="" class="button display-block text-center hollow">Date Posted</a>
+		</div>
+		<!-- End sort by Modal -->
+		
+		<!-- Sort by Location -->
+		
 <!-- Ad Content -->
 
 		<div class="adListings">
@@ -51,7 +76,34 @@
 			</div>
 			<?php } ?>
 			
+			
 		</div>
+		<a href="#" data-reveal-id="firstModal" class="radius button">Modal in a modal&hellip;</a>
+<a href="#" data-reveal-id="videoModal" class="radius button">Example Modal with Video&hellip;</a>
+
+<!-- Reveal Modals begin -->
+<div id="firstModal" class="reveal-modal" data-reveal aria-labelledby="firstModalTitle" aria-hidden="true" role="dialog">
+  <h2 id="firstModalTitle">This is a modal.</h2>
+  <p>Reveal makes these very easy to summon and dismiss. The close button is simply an anchor with a unicode character icon and a class of <code>close-reveal-modal</code>. Clicking anywhere outside the modal will also dismiss it.</p>
+  <p>Finally, if your modal summons another Reveal modal, the plugin will handle that for you gracefully.</p>
+  <p><a href="#" data-reveal-id="secondModal" class="secondary button">Second Modal...</a></p>
+  <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<div id="secondModal" class="reveal-modal" data-reveal aria-labelledby="secondModalTitle" aria-hidden="true" role="dialog">
+  <h2 id="secondModalTitle">This is a second modal.</h2>
+  <p>See? It just slides into place after the other first modal. Very handy when you need subsequent dialogs, or when a modal option impacts or requires another decision.</p>
+  <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
+
+<div id="videoModal" class="reveal-modal large" data-reveal aria-labelledby="videoModalTitle" aria-hidden="true" role="dialog">
+  <h2 id="videoModalTitle">This modal has video</h2>
+  <div class="flex-video widescreen vimeo">
+    <iframe width="1280" height="720" src="//www.youtube-nocookie.com/embed/wnXCopXXblE?rel=0" frameborder="0" allowfullscreen></iframe>
+  </div>
+
+  <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+</div>
 		<a class="exit-off-canvas"></a>
 <!-- end of ad content -->
 
