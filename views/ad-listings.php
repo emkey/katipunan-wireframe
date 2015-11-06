65661b2dba4b19623025889d006b827ae07f653d
emkey <mllacsamana@gmail.com>
2015-11-06 18:02:23 +0800

    Added variations to ad listings

--- views/ad-listings.php
+++ views/ad-listings.php
@@ -17,14 +17,66 @@
 					<li><a href="#" data-reveal-id="filterModal-appPush">Advanced Filter</a></li>
 				</ul>
 			</aside>
-		
-			<!-- Ad Content -->
+
+
+			<?php
+				$page = $_GET['page'];	/* gets the variable $page */
+				if ($page == "no-results"){
+			?>
+			<div class="search-meta row">
+				<div class="small-12 columns">
+					<h2><small>No results for &ldquo;Phones&rdquo;</small></h2>
+					<small>Do you mean: &quot;<a href="">Hondo</a>&quot;&#63;</small>
+				</div>
+			</div>
+			<div class="adListings">
+				<div class="small-12 column padding-top-half">
+					<small>Sponsored link related to: <strong>condo</strong></small>
+				</div>
+				<div class="adSense-item">
+					<a href="#">
+					<div class="card">
+						<div class="adMeta">
+							<h3><small>Adsense Ad Title</small></h3>
+							<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </small>
+						</div>
+					</div>
+					</a>
+				</div>
+			</div>
+			<?php
+				} 	/* if $page has a value, include it */
+				else if ($page == "foreign"){
+			?>
 			<div class="search-meta row">
 				<div class="small-12 columns">
 				<h2><small>18 results for &ldquo;Phones&rdquo;</small></h2>
 				</div>
 			</div>
 			<div class="adListings">
+				<div class="adSense-item">
+					<a href="#">
+					<div class="card">
+						<div class="adMeta">
+							<h3><small>Adsense Ad Title</small></h3>
+							<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </small>
+						</div>
+					</div>
+					</a>
+				</div>
+				<div class="adListing-item sponsored">
+					<a href="#">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+							
+						</div>
+					</div>
+					</a>
+				</div>
 				<div class="adListing-item sponsored">
 					<a href="#">
 					<div class="card">
@@ -39,7 +91,7 @@
 					</a>
 				</div>
 				<?php 
-				for ($i = 0; $i < 7; $i++) {	
+				for ($i = 0; $i < 6; $i++) {	
 				?>
 				<div class="adListing-item">
 					<a href="#">
@@ -60,7 +112,7 @@
 					</div>
 				</div>
 				<?php 
-				for ($i = 0; $i < 7; $i++) {	
+				for ($i = 0; $i < 6; $i++) {	
 				?>
 				<div class="adListing-item">
 					<div class="card">
@@ -83,7 +135,31 @@
 						</div>
 					</div>
 				</div>
-				
+				<div class="adListing-item sponsored">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+						</div>
+					</div>
+				</div>
+				<div class="small-12 column">
+					<div class="adbox display-block text-center">
+						Ad Box
+					</div>
+				</div>
+				<div class="adSense-item">
+					<a href="#">
+					<div class="card">
+						<div class="adMeta">
+							<h3><small>Adsense Ad Title</small></h3>
+							<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </small>
+						</div>
+					</div>
+					</a>
+				</div>
 			</div>
 			<nav class="row">
 				<div class="small-6 medium-4 large-3 column text-center">
@@ -93,14 +169,125 @@
 					<a href="#" class="button display-block hollow">Next</a>
 				</div>
 			</nav>
+			<?php
+				}
+				else {
+			?>
+			<div class="search-meta row">
+				<div class="small-12 columns">
+				<h2><small>18 results for &ldquo;Phones&rdquo;</small></h2>
+				</div>
+			</div>
+			<div class="adListings">
+				<div class="adListing-item sponsored">
+					<a href="#">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+							
+						</div>
+					</div>
+					</a>
+				</div>
+				<div class="adListing-item sponsored">
+					<a href="#">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+							
+						</div>
+					</div>
+					</a>
+				</div>
+				<?php 
+				for ($i = 0; $i < 6; $i++) {	
+				?>
+				<div class="adListing-item">
+					<a href="#">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+						</div>
+					</div>
+					</a>
+				</div>
+				<?php } ?>
+				<div class="small-12 column">
+					<div class="adbox display-block text-center">
+						Ad Box
+					</div>
+				</div>
+				<?php 
+				for ($i = 0; $i < 6; $i++) {	
+				?>
+				<div class="adListing-item">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+						</div>
+					</div>
+				</div>
+				<?php } ?>
+				<div class="adListing-item sponsored">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+						</div>
+					</div>
+				</div>
+				<div class="adListing-item sponsored">
+					<div class="card">
+						<div class="adThumbnail" style="background-image: url('http://placehold.it/640x360')">
+						</div>
+						<div class="adMeta">
+							<h3><small>Hello</small></h3>
+							<strong class="price">P1200</strong>
+						</div>
+					</div>
+				</div>
+				<div class="small-12 column">
+					<div class="adbox display-block text-center">
+						Ad Box
+					</div>
+				</div>
+			</div>
+			<nav class="row">
+				<div class="small-6 medium-4 large-3 column text-center">
+					<a href="#" class="button display-block hollow">Previous</a>
+				</div>
+				<div class="small-6 medium-4 large-3 column text-center">
+					<a href="#" class="button display-block hollow">Next</a>
+				</div>
+			</nav>	
+			<?php
+				} 	/* otherwise, include the default page */
+				
+			?>
+			<!-- Ad Content -->
+			
 			<div class="row margin-top-medium">
 				<div class="small-12 column">
 					<a href="#" class="left-off-canvas-toggle filter-button button display-block text-center">Filter</a>
 				</div>
 			</div>
-
 			<a class="exit-off-canvas"></a>
 			<!-- end of ad content -->
+			
 			<?php include_once ('modals.php');?>
 			<?php include('footer.php'); ?>
 		</div>
